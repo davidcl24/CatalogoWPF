e5ee5075911e79c189ff6c5fee75176534dc2f15
davidcl24 <davidcl.flor@gmail.com>
2025-01-17 09:47:29 +0100

    Añadida conexion DB en productos

--- Services-Repos/Models/Repositories/ProductRepository.cs
+++ Services-Repos/Models/Repositories/ProductRepository.cs
@@ -1,28 +1,31 @@
-﻿using Services_Repos.Models.Data_Classes;
+﻿using Services_Repos.Data;
+using Services_Repos.Models.Data_Classes;
 
 namespace Services_Repos.Models.Repositories;
 
-public class ProductRepository : IRepository<Product>
+public class ProductRepository(AppDbContext dbContext) : IRepository<Product>
 {
+    private readonly AppDbContext _dbContext = dbContext;
     private List<Product> _products = [];
-    public void Add(Product item) => _products.Add(item);
+    public void Add(Product item) => _dbContext.Add(item);
 
-    public List<Product> GetAll() => _products;
+    public List<Product> GetAll() => [.. _dbContext.Products];
 
-    public Product GetById(int id) => _products.Find(p => p.Id == id);
+    public Product GetById(int id) => _dbContext.Find<Product>(id);
 
-    public void Remove(Product item) => _products.Remove(item);
+    public void Remove(Product item) => _dbContext.Remove(item);
 
     public void Update(Product item)
     {
-        Product? product = _products.Find(p => p.Id == item.Id);
-        if (product != null)
-        {
-            product.Id = item.Id;
-            product.Name = item.Name;
-            product.Description = item.Description;
-            product.Price = item.Price;
-        }
+        _dbContext.Update(item);
+        //Product? product = _products.Find(p => p.Id == item.Id);
+        //if (product != null)
+        //{
+        //    product.Id = item.Id;
+        //    product.Name = item.Name;
+        //    product.Description = item.Description;
+        //    product.Price = item.Price;
+        //}
     }
 }
 
