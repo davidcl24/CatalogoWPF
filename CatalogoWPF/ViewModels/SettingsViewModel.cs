566f9ecc153c00facaaebc6df397f89bb767ebbf
davidcl24 <davidcl.flor@gmail.com>
2025-01-27 11:54:47 +0100

    optimizacion de usings

--- CatalogoWPF/ViewModels/SettingsViewModel.cs
+++ CatalogoWPF/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,6 @@
-﻿using CatalogoWPF.Views;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Services_Repos.Services;
 using System.Collections.ObjectModel;
-using System.Configuration;
-using System.Data;
-using System.Globalization;
 using System.Windows;
 
 namespace CatalogoWPF.ViewModels;
